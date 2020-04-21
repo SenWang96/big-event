cfdd2800eb31455d6a316f83ee626cd7b3953b3c
Laotang1234 <tangfengpo@163.com>
2020-04-21 14:54:21 +0800

    完成了注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,4 +11,24 @@ $(function () {
         $('.register').hide().prev().show();
     });
 
+    // --------------------- 完成注册功能 ---------------------
+    $('.register form').on('submit', function (e) {
+        e.preventDefault();
+        // 获取表单中的数据
+        let data = $(this).serialize(); // serialize是根据表单各项的name属性获取值的，所以要检查表单各项的name属性
+        // 发送ajax请求到接口，完成注册
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/api/reguser',
+            data: data,
+            success: function (res) {
+                alert(res.message);
+                if (res.status === 0) {
+                    // 注册成功，让登陆的盒子显示
+                    $('.register').hide().prev().show();
+                }
+            }
+        });
+    });
+
 });
