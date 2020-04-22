f4f0d83bc6a7fc0a58158413a2047591fb39c466
Laotang1234 <tangfengpo@163.com>
2020-04-22 16:50:12 +0800

    完成了更新用户信息的功能

--- assets/js/user/userinfo.js
+++ assets/js/user/userinfo.js
@@ -23,4 +23,35 @@ $(function () {
         });
     }
 
+
+    // ------------------   表单提交的时候，完成用户信息的更新 -----------------
+    // 1. 注册表单的提交事件
+    $('form').on('submit', function (e) {
+
+        // 2. 阻止默认行为
+        e.preventDefault();
+        // 3. 收集表单数据
+        let data = form.val('user');
+        // console.log(data); // {id: "900", username: "abcdabcd", nickname: "你好123", email: "232323@qq.comadsf"}
+        // 删除掉username，因为接口不需要这个参数。不过这个接口测试过，传过去也没事
+        // 4. ajax提交数据给接口
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/my/userinfo',
+            data: data,
+            headers: {
+                Authorization: localStorage.getItem('token')
+            },
+            success: function (res) {
+                // console.log(res);
+                if (res.status === 0) {
+                    // 5. 修改成功，给出提示，调用父页面的getUserInfo函数，从新渲染index.html 
+                    layer.msg(res.message);
+                    // 调用父页面的getUserInfo函数，从新渲染index.html 
+                    window.parent.getUserInfo();
+                }
+            }
+        });
+    });
+
 });
